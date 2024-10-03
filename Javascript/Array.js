f618c5a926159c98831f93280034b10d3a30346d
SNS1608 <sandeep.seeram1@gmail.com>
2024-10-03 21:32:24 +0530

    add the folder

---
+++ Javascript/Array.js
@@ -0,0 +1,13 @@
+let vlaues=[10,20,30,40,50];
+let marvel=["ironman","spidy","wanda"];
+
+console.log(vlaues.pop());
+console.log(vlaues.push("100"));
+console.log(vlaues);
+console.log(vlaues.toString());
+let con =vlaues.concat(marvel);
+console.log(con);
+console.log(vlaues.shift());
+console.log(vlaues.unshift("sandeep"));
+console.log(vlaues);
+console.log(vlaues.slice(1,4));
---
+++ Javascript/index.js
@@ -0,0 +1,93 @@
+// // data types
+
+// let san= "sandeep";
+// let number=123456;
+// let bool= true;
+// let UD;
+// let ndll= null;
+
+// console.log(san,number,bool,UD,ndll);
+// --------------------------------------------------------//
+// operator -arthmetic, conditional,assignment,logical operator
+let num1=10;
+let num2=13;
+// console.log(num1+num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
+// console.log(num1%num2);
+// ---------------------------------------
+//  conditional
+// if(num1===num2){
+//     console.log("Both are equal");
+// }else{
+//     console.log("both are not same");
+// }
+// --------------------------------------------------
+// // terminatory operator
+// let num=13;
+//  let res=(num%2==0)?"even": "odd";
+// console.log(res);
+// -------------------else if--------------------
+// let percentage=prompt("enter the percentage");
+// if(percentage>90){
+//     console.log("you are passed with Grade A");
+// }else if(percentage>80){
+//     console.log("you are passed with grade B");
+// }else{
+//     console.log("You entered the wrong number");
+// }
+// ----------------Switch-------------
+// /let op =prompt("enter the  operator +,-,/,%,*");
+// let num3=10;
+// let num4=20;
+
+// switch(op){
+//     case "+":
+//         console.log(num3+num4);
+//         break;
+//     case "-":
+//         console.log(num3-num4);
+//         break;
+//     case "*":
+//         console.log(num3*num4);
+//         break;
+//      case "/":
+//             console.log(num3-num4);
+//             break;
+//      case "-":
+//                 console.log("enter the correct symbol")
+                
+
+
+// }
+// ----------------------for loop------------------------------
+
+// let arr=[1,2,3,4];
+// sum=0;
+// for(let i=0;i<arr.length;i++){
+//     sum+= arr[i];
+//     // console.log(sum);
+// }
+// console.log(sum);
+//----------------------for of---------------------------
+// let arr=[1,2,3,4];
+// for(let x of arr){
+//     console.log(x);
+// }
+// ----------------------for in------------
+// let arr=[1,2,3,4];
+// for(let x in arr){
+//     console.log(x);
+// }
+
+// ----------for each----------------
+// let arr=[1,2,3,4];
+// let myarr= arr.forEach(value => {
+//     if(value == 2){
+//         console.log(value);
+//     }  
+// });
+
+
+
+
